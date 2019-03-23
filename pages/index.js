50cb750972b8e1d75aed8afb7e2f8e53a181f3d9
Joshua Martinez <joshkmartinez@gmail.com>
2019-03-22 21:57:04 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import { Flex, Text } from 'rebass'
+const Index = () => (
+  <React.Fragment>
+    <Head>
+      <title>💩</title>
+    </Head>
+    <Flex alignItems="center" justifyContent="center">
+      <Text fontSize={[420,369,69]}>💩</Text>
+    </Flex>
+  </React.Fragment>
+)
+
+export default Index
